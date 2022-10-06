3070b7410a2abd3ba464a2fbffe1dea907f62d44
110306033劉威廷 <110306033@nccu.edu.tw>
2022-10-06 09:18:30 +0800

    second try

--- DataFinalProject/src/main/java/com/example/datafinalproject/Hello.java
+++ DataFinalProject/src/main/java/com/example/datafinalproject/Hello.java
@@ -2,6 +2,6 @@ package com.example.datafinalproject;
 
 public class Hello {
     public static void main(String[]args){
-        System.out.print("Helo");
+        System.out.print("Hello Git");
     }
 }
